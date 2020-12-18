5221f46382b6a84742623cbc2cc1bf179e576a4a
Smriti111 <smritikpriya@gmail.com>
2020-12-18 13:23:42 +0530

    added the code for code for text extraction from a pdf file

--- txt_to_speech.py
+++ txt_to_speech.py
@@ -1,1 +1,21 @@
-venv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import PyPDF2
+from gtts import gTTS
+Tk().withdraw()
+filelocation = askopenfilename(initialdir = "/desktop/pdf_reader",title = "Select file",filetypes = (("pdf files","*.pdf"),("all files","*.*")))
+with open(filelocation,'rb') as of:
+    pdf=PyPDF2.PdfFileReader(of)
+    page_number=0
+    txt=''
+    while page_number<pdf.getNumPages():
+        page_data=pdf.getPage(page_number)
+        txt+=page_data.extractText()
+        page_number+=1
+print(txt)
+final_file = gTTS(text=txt, lang='en') 
+final_file.save("Generated_Speech.mp3")  # save file to computer
+print("Task Completed!!")
+
+
+
